2a1e399ca08671212373a26e6caa48acb29b1552
qingyin <ouyangshima@163.com>
2017-02-09 18:47:24 +0800

    add info

--- Work/H5Work/MyCoreCocosJS/src/core/loader/LoaderManager.js
+++ Work/H5Work/MyCoreCocosJS/src/core/loader/LoaderManager.js
@@ -15,6 +15,35 @@ core.LoaderManager.loadFileSync = function (filePath) {
     return txtData;
 }
 
+core.LoaderManager.loadImgFromUrl = function (targetSprite, imgUrl, p, tag) {
+    if(!imgUrl)return;  
+    var self = targetSprite;  
+    var loadCb = function(err, img){
+        if(err) {
+            cc.log("err------",err);
+            return;
+        }
+        cc.textureCache.addImage(imgUrl);
+
+        if (cc.sys.isNative) {
+            self.initWithTexture(img);
+            if(self.imgH && self.imgW){            	
+	            self.setScaleY(self.imgH/img.height);
+	            self.setScaleX(self.imgW/img.width);
+            }
+        }else{
+            var texture2d = new cc.Texture2D();          
+            texture2d.initWithElement(img);
+            texture2d.handleLoadedTexture();
+            self.initWithTexture(texture2d);
+            if(self.imgH && self.imgW){            	
+	            self.setScaleY(self.imgH/img.height);
+	            self.setScaleX(self.imgW/img.width);
+            }
+        }  
+    };  
+    cc.loader.loadImg(imgUrl, {isCrossOrigin:true}, loadCb);
+}
 
 
 
