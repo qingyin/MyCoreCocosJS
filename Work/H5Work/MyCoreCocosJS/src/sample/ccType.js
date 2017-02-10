0229d9a32c0fd6ea945f67394d5b81a5ef706af6
qingyin <ouyangshima@163.com>
2017-02-10 10:58:13 +0800

    update

---
+++ Work/H5Work/MyCoreCocosJS/src/sample/ccType.js
@@ -0,0 +1,9 @@
+ 
+ // 1. All channels separately as parameters
+ var color1 = cc.color(255, 255, 255, 255);
+ 
+ // 2. Convert a hex string to a color
+ var color2 = cc.color("#000000");
+ 
+ // 3. An color object as parameter
+ var color3 = cc.color({r: 255, g: 255, b: 255, a: 255});
